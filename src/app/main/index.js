24a6904a0739f30db6fd0e5a343f1012e10ad78d
ARognes <acrognes@iastate.edu>
2022-03-14 15:03:23 -0500

    code pdf format 1"x1.5"

--- src/app/main/index.js
+++ src/app/main/index.js
@@ -39,8 +39,6 @@ pTitle.innerHTML = businessName || '';
 
 btnGenerate.onclick = async () => {
 
-  console.log(businessID);
-
   if (!businessID) {
     console.error("BusinessID not defined");
     return;
@@ -49,8 +47,110 @@ btnGenerate.onclick = async () => {
   const queryEquipment = await getDocs(query(collection(db, 'equipment'), where('businessID', '==', businessID)));
 
   const equipment = getQueryData(queryEquipment);
+  equipment.sort((a, b) => a.name > b.name);
 
+  savePDF(equipment);
+}
+
+function savePDF(equipment) {
+
+  const pdf = new jsPDF({ unit: 'in' });
+  pdf.text('Equipment codes: ' + businessName, 0.12, 0.3);
+  pdf.setLineWidth(0.1);
+  pdf.setFontSize(6);
+
+  // Print qr codes
+  let equipmentIndex = 0;
+  let printingQRCodes = true;
+  let canvas = document.createElement('canvas');
+  while (equipmentIndex < equipment.length) {
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 5; j++) {
+
+        if (printingQRCodes) {
+          QRCode.toCanvas(canvas, equipment[equipmentIndex].id, (error) => error ? console.error(error) : null);
+          let x = 1.5 + j * 1.5;
+          let y = 0.5 + i;
+          pdf.addImage(canvas, "JPEG", x, y, 1, 1, '', 'NONE', 90);
+          canvas.width = 1000;
+          canvas.height = 420;
+          let ctx = canvas.getContext('2d');
+          ctx.fillStyle = 'white';
+          ctx.fillRect(0, 0, canvas.width, canvas.height);
+          ctx.font = '80px monospace';
+          ctx.textAlign = 'center';
+          ctx.fillStyle = 'black';
+          ctx.strokeStyle = 'black';
+          ctx.lineWidth = 2;
+
+          let restOfName = equipment[equipmentIndex].name;
+          let k = 0;
+          while (restOfName) {
+            let name = restOfName;
+            if (restOfName.length >= 18) {
+              name = restOfName.substring(0, 18);
+              restOfName = restOfName.substring(18);
+            }
+            else restOfName = null;
+            ctx.fillText(name, canvas.width / 2, 80 * k + 60);
+            k++;
+          }
+          
+          pdf.addImage(canvas, "JPEG", x + 0.35, y + 0.58, 1, 0.42, '', 'NONE', 90);
+
+        } else {
+          JsBarcode(canvas, equipment[equipmentIndex].barcode, {format: "CODE128", width: 12, height: 500, fontSize: 80});
+          let x = 0.6 + j * 1.5;
+          let y = 0.6 + i;
+          pdf.addImage(canvas, "JPEG", x, y, 1.3, 0.63523);
+
+          canvas.width = 1500;
+          canvas.height = 630;
+          let ctx = canvas.getContext('2d');
+          ctx.fillStyle = 'white';
+          ctx.fillRect(0, 0, canvas.width, canvas.height);
+          ctx.font = '80px monospace';
+          ctx.textAlign = 'center';
+          ctx.fillStyle = 'black';
+          ctx.strokeStyle = 'black';
+          ctx.lineWidth = 2;
+
+          const CUT_LEN = 30;
+          ctx.fillText(equipment[equipmentIndex].name.substring(0, CUT_LEN), canvas.width / 2, 60);
+          if (equipment[equipmentIndex].name.length > CUT_LEN) ctx.fillText(equipment[equipmentIndex].name.substring(CUT_LEN), canvas.width / 2, 140);
+          
+          pdf.addImage(canvas, "JPEG", x - 0.1, y + 0.66, 1.5, 0.63);
+
+        }
+
+        // pdf.line(0.5 + j * 1.5, 0.5 + i, 2 + j * 1.5, 0.5 + i);
+        // pdf.line(0.5 + j * 1.5, 1.5 + i, 2 + j * 1.5, 1.5 + i);
+        // pdf.line(0.5 + j * 1.5, 0.5 + i, 0.5 + j * 1.5, 1.5 + i);
+        // pdf.line(2 + j * 1.5, 0.5 + i, 2 + j * 1.5, 1.5 + i);
+        
+        equipmentIndex++;
+        
+        if (equipmentIndex >= equipment.length) {
+          if (printingQRCodes) {
+            printingQRCodes = false;
+            equipmentIndex = 0;
+          } 
+          else {
+            pdf.save("Equipment Codes.pdf");
+            return;
+          }
+        }
+      }
+    }
+    pdf.addPage();
+  }
+
+  pdf.save("Equipment Codes.pdf");
+}
+
+function savePDFHuman(equipment) {
   const pdf = new jsPDF();
+
   pdf.setFontSize(12);
   pdf.text("Equipment-Record: " + businessName, 10, 10);
 
@@ -78,7 +178,6 @@ btnGenerate.onclick = async () => {
       pdf.text("Equipment-Record: " + businessName, 10, 10);
     }
   }
-
   pdf.save("Equipment Codes.pdf");
 }
 
