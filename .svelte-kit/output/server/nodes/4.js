192d384fbbace9bdef2deb01eaa4ba0ff1cc4e72
ARognes <acrognes@iastate.edu>
2022-06-24 23:12:55 -0500

    try heroku

--- .svelte-kit/output/server/nodes/4.js
+++ .svelte-kit/output/server/nodes/4.js
@@ -3,4 +3,4 @@ import * as module from '../entries/pages/forgot.svelte.js';
 export { module };
 export const entry = 'pages/forgot.svelte-a5ccd87f.js';
 export const js = ["pages/forgot.svelte-a5ccd87f.js","chunks/vendor-7483cdb5.js"];
-export const css = ["assets/pages/business.svelte-84a610ef.css","assets/vendor-dc74a490.css"];
+export const css = ["assets/pages/forgot.svelte-9f6c3866.css","assets/vendor-dc74a490.css"];
