121946b2db46755258892749711c8fbbf5b999e2
ARognes <acrognes@iastate.edu>
2022-06-24 23:02:00 -0500

    script loading try 17

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,6 @@
-// import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-auto";
 import preprocess from "svelte-preprocess";
 import svg from "@poppanator/sveltekit-svg";
-
-import vercel from '@sveltejs/adapter-vercel';
- 
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,20 +9,7 @@ const config = {
   preprocess: preprocess(),
 
   kit: {
-    adapter: vercel({
-      // if true, will deploy the app using edge functions
-      // (https://vercel.com/docs/concepts/functions/edge-functions)
-      // rather than serverless functions
-      edge: false,
-
-      // an array of dependencies that esbuild should treat
-      // as external when bundling functions
-      external: [],
-
-      // if true, will split your app into multiple functions
-      // instead of creating a single one for the entire app
-      split: false
-    }),
+    adapter: adapter(),
     // This fixes a bug: https://github.com/sveltejs/kit/issues/2418
     vite: {
       ssr: {
