7205c573a2f6f53e789436698129a4513ea1596c
ARognes <acrognes@iastate.edu>
2022-07-11 22:03:03 -0500

    customClaims check 7

--- preprocess.sh
+++ preprocess.sh
@@ -3,14 +3,13 @@
 # Friendly-challenge patch
 sed -i -e 's/"dependencies": {/"type": "module", "dependencies":  {/1' ./node_modules/friendly-challenge/package.json
 
-ls -la
-pwd
-# Expose .env variables to bash
-set -o allexport   
-source .env set
+# Expose .env variables to bash, Vercal will have these pre-exposed
+# set -o allexport   
+# source .env set
 
 echo $FIREBASE_ADMIN_CREDENTIALS > gcloud-service-account.json
 echo $FIREBASE_ADMIN_CREDENTIALS
+cat gcloud-service-account.json
 
 export GOOGLE_APPLICATION_CREDENTIALS=gcloud-service-account.json
 
