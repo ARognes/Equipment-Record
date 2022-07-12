b19a09eea3e49558d48936ce74192a01909b0e8d
ARognes <acrognes@iastate.edu>
2022-07-11 21:37:50 -0500

    customClaims check 6

--- preprocess.sh
+++ preprocess.sh
@@ -3,11 +3,14 @@
 # Friendly-challenge patch
 sed -i -e 's/"dependencies": {/"type": "module", "dependencies":  {/1' ./node_modules/friendly-challenge/package.json
 
+ls -la
+pwd
 # Expose .env variables to bash
 set -o allexport   
 source .env set
 
 echo $FIREBASE_ADMIN_CREDENTIALS > gcloud-service-account.json
+echo $FIREBASE_ADMIN_CREDENTIALS
 
 export GOOGLE_APPLICATION_CREDENTIALS=gcloud-service-account.json
 
